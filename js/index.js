6b324c27bd0b6d7ff935386192e7cfc4bc68f2aa
NguyenQuoc <nguyenx2quoc@gmail.com>
2017-04-23 18:57:56 +0700

    Add new project

---
+++ js/index.js
@@ -0,0 +1,26 @@
+var routerApp = angular.module('routerApp', ['ui.router']);
+
+routerApp.config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/home');
+    $stateProvider   .state('home', {
+        url:'/home' ,
+        templateUrl: 'HomePage.html',
+
+    });
+    $stateProvider  .state('create', {
+        url:'/create' ,
+        templateUrl: 'CreateItem.html',
+    });
+    $stateProvider  .state('error', {
+        url: '/404',
+        templateUrl: 'error-404.html'
+    });
+    $stateProvider  .state('detail', {
+        url:'/detail' ,
+        templateUrl: 'DetailItem.html',
+    });
+    $stateProvider  .state('search', {
+        url:'/search' ,
+        templateUrl: 'Result.html',
+    });
+});

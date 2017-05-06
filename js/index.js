2ce4e78280729d3ae834e1835fb435c0ba91d1b9
Lê Tuấn Tài <tuantai0625@gmail.com>
2017-05-06 12:10:01 +0700

    Improve header design

--- js/index.js
+++ js/index.js
@@ -68,3 +68,13 @@ function slideBack() {
 function slideNext() {
   showSlider(curIndex + 1);
 }
+
+$(document).ready(function(e){
+    $('.search-panel .dropdown-menu').find('a').click(function(e) {
+        e.preventDefault();
+        var param = $(this).attr("href").replace("#","");
+        var concept = $(this).text();
+        $('.search-panel span#search_concept').text(concept);
+        $('.input-group #search_param').val(param);
+    });
+});
